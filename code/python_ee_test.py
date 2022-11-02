bdc47c613667b33949b983b30b910e3f46c8c4a0
Ryan Ing <s1834371@ed.ac.uk>
2022-11-02 16:39:01 +0000

    edited ee code

--- code/python_ee_test.py
+++ code/python_ee_test.py
@@ -18,11 +18,14 @@ import geemap
 # =============================================================================
 ee.Initialize()
 
-ROI_dir = 'C:/Users/s1834371/Documents/PhD/GFSTS/Code/GIS/ROI/ROI.geojson'
+data_dir = 'C:/Users/s1834371/Documents/PhD/SENSE_training/Bogs' # direct of github repo
+
+ROI_dir = data_dir + '/ROI/BogsOnFire_ROI.geojson' # ROI of wildfire region
 ROI = geemap.geojson_to_ee(ROI_dir)
 
 #Filtering sentinel-2 TOA to cloud percentage, rough greenland ROI and timeframe
 s2_dataset = ee.ImageCollection('COPERNICUS/S2_HARMONIZED')\
                 .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))\
                 .filter(ee.Filter.bounds(ROI))\
-                .filterDate('2015-06-01', '2022-10-01')
+                .filterDate('2019-04-01', '2022-10-01') # Filtering to study time period
+            
