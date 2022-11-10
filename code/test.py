f9733612829c690b948109ee188c833139ad0ec2
ry-ing <ryan.ing@outlook.com>
2022-11-10 16:17:51 +0000

    edited NDWI code

--- code/test.py
+++
@@ -1,5 +0,0 @@
-import ee 
-import geemap
-
-ee.Initialize()
-ROI = geemap.geojson_to_ee(r'C:/Users/s1834371/Documents/PhD/SENSE_training/Bogs/ROI/BogsOnFire_ROI.geojson')
--- code/time_series_analysis_fft.py
+++ code/time_series_analysis_fft.py
@@ -23,46 +23,41 @@ plt.rcParams["ytick.major.size"] = 8
 plt.rcParams["xtick.minor.size"] = 4
 plt.rcParams["ytick.minor.size"] = 4
 
-
-
-
 #--------------import data----------------------------------
-data = pd.read_csv(r'C:\Users\s1834371\Documents\PhD\SENSE_training\Bogs\code\ndvi_ndwi_data_v2.csv')
+data = pd.read_csv(r'C:\Users\s1834371\Documents\PhD\SENSE_training\Bogs\code\NDVI_NDWI_data_v2.csv')
 data = data.dropna()
 date_dt = pd.to_datetime(data['date'], dayfirst=True, format='%d/%m/%Y')
 date = date_dt.values
 ndvi = data['NDVI'].values
 ndwi = data['NDWI'].values
 
-data_wider = pd.read_csv(r'C:\Users\s1834371\Documents\PhD\SENSE_training\Bogs\code\ndvi_ndwi_wider_data.csv')
+data_wider = pd.read_csv(r'C:\Users\s1834371\Documents\PhD\SENSE_training\Bogs\code\NDVI_NDWI_wide_data_v2.csv')
 data_wider = data_wider.dropna()
 ndvi_wider = data_wider['NDVI'].values
 ndwi_wider = data_wider['NDWI'].values
-
 
 #-----------FFT----------------------------
 fft = np.fft.fft(ndwi_wider)
 fft_abs = np.abs(fft)
 
-cutoff = 10
+cutoff = 12
 fft[cutoff:] = 0
 new_ndwi = np.fft.ifft(fft)
 
 anomaly = (ndwi - new_ndwi)
-print(date)
-#sys.exit()
 
-
+#===================================creating figure=======================================================
 fig = plt.figure()
 spec = gridspec.GridSpec(ncols=1, nrows=2, height_ratios=[1.5,1], hspace=0.1)
 #-----------------------------ax1----------------------------------------
 ax1 = fig.add_subplot(spec[0])
 ax1.plot(date, new_ndwi, label='Background NDWI', color='k', linestyle='--')
-ax1.plot(date, ndwi, label='NDWI', color='b')
-ax1.plot(date, ndvi, label='NDVI', color='limegreen', zorder=-1)
+ax1.plot(date, ndwi, label='NDWI burnt', color='b')
+ax1.plot(date, ndvi, label='NDVI burnt', color='limegreen', zorder=-1)
 
 ax1.vlines(x=pd.to_datetime('2019-05-01'), ymin=-1, ymax=1, colors='r', zorder=-4)
 ax1.text(x=pd.to_datetime('2019-05-09'), y=0.75, s='Wildfire (16th May 2019)', color='red')
+ax1.set_title('NWDI and NDVI')
 #---formatting dates--------------------------------------
 ax1.xaxis.set_minor_locator(mdates.MonthLocator(interval=1))
 ax1.xaxis.set_major_locator(mdates.YearLocator())
@@ -90,16 +85,13 @@ ax2.tick_params(axis='x', which='both', rotation=90)
 ax2.set_xlim(date[0], date[-1])
 
 ax2.vlines(x=pd.to_datetime('2019-05-01'), ymin=-1, ymax=1, colors='r', zorder=-4)
-#ax2.text(x=pd.to_datetime('2019-05-09'), y=0.55, s='Wildfire (16th May 2019)', color='red')
 
-
-
-figure_filename = 'ndvi_ndwi_figure.png'
-
+#----------saving figure---------------------
+figure_filename = r'code\ndvi_ndwi_figure.png'
 width = 6.88 + 4
 height = width/1.618 - 2
-
 
 fig.set_size_inches(width, height)
 fig.savefig(figure_filename, dpi=300,  bbox_inches='tight')
 plt.show()
+
