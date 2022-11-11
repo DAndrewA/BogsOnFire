cc7c7c351c53bb271a078210fabb60faee57afd3
ry-ing <ryan.ing@outlook.com>
2022-11-11 12:06:47 +0000

    edited timeseries figure

--- code/time_series_analysis_fft.py
+++ code/time_series_analysis_fft.py
@@ -22,6 +22,8 @@ plt.rcParams["xtick.major.size"] = 8
 plt.rcParams["ytick.major.size"] = 8
 plt.rcParams["xtick.minor.size"] = 4
 plt.rcParams["ytick.minor.size"] = 4
+#plt.rcParams['axes.facecolor'] = 'black'
+
 
 #--------------import data----------------------------------
 data = pd.read_csv(r'C:\Users\s1834371\Documents\PhD\SENSE_training\Bogs\code\NDVI_NDWI_data_v2.csv')
@@ -48,16 +50,24 @@ anomaly = (ndwi - new_ndwi)
 
 #===================================creating figure=======================================================
 fig = plt.figure()
-spec = gridspec.GridSpec(ncols=1, nrows=2, height_ratios=[1.5,1], hspace=0.1)
-#-----------------------------ax1----------------------------------------
-ax1 = fig.add_subplot(spec[0])
-ax1.plot(date, new_ndwi, label='Background NDWI', color='k', linestyle='--')
-ax1.plot(date, ndwi, label='NDWI burnt', color='b')
-ax1.plot(date, ndvi, label='NDVI burnt', color='limegreen', zorder=-1)
+spec = gridspec.GridSpec(ncols=1, nrows=2, height_ratios=[1.5,1], hspace=0.05)
 
-ax1.vlines(x=pd.to_datetime('2019-05-01'), ymin=-1, ymax=1, colors='r', zorder=-4)
-ax1.text(x=pd.to_datetime('2019-05-09'), y=0.75, s='Wildfire (16th May 2019)', color='red')
-ax1.set_title('NWDI and NDVI')
+#fig.patch.set_facecolor('black')
+
+#-----------------------------ax1----------------------------------------
+#plt.style.use('dark_background')
+ax1 = fig.add_subplot(spec[0])
+ax1.plot(date, new_ndwi, label='Background NDWI', color='deepskyblue', linestyle='--', linewidth=1.8)
+ax1.plot(date, ndwi, label='NDWI burnt', color='darkblue', linewidth=1.8)
+ax1.scatter(date, ndwi, color='darkblue', s=10, marker='^')
+ax1.plot(date, ndvi, label='NDVI burnt', color='forestgreen', zorder=-1, linewidth=1.8)
+ax1.scatter(date, ndvi, color='forestgreen', s=10, marker='o')
+
+ax1.vlines(x=pd.to_datetime('2019-05-01'), ymin=-1, ymax=1, colors='r', linewidth=11.0, alpha=0.6, zorder=-4)
+ax1.hlines(y=0, xmin=date[0], xmax=date[-1], color='k', linewidth=0.8, zorder=-4)
+ax1.text(x=pd.to_datetime('2019-05-23'), y=0.75, s='Wildfire (12th May 2019)', color='red')
+
+ax1.set_title('NWDI and NDVI timeseries of the burnt peatland area', fontsize=17)
 #---formatting dates--------------------------------------
 ax1.xaxis.set_minor_locator(mdates.MonthLocator(interval=1))
 ax1.xaxis.set_major_locator(mdates.YearLocator())
@@ -66,17 +76,29 @@ ax1.legend()
 ax1.set_xlim(date[0], date[-1])
 ax1.set_ylim(-0.3,0.9)
 ax1.set_ylabel('Index Value')
+
+
+leg = ax1.legend(loc='lower right', ncol=3, handlelength=4.5, fontsize=tick_fontsize, columnspacing=2)
+for legobj in leg.legendHandles:
+    legobj.set_linewidth(4.0)
 #-------------------------------ax2-------------------------------------------
 ax2 = fig.add_subplot(spec[1])
 
-ax2.bar(date_dt, anomaly, width=30, color='grey', edgecolor='k')
+ax2.bar(date_dt, anomaly, width=30, color='purple', edgecolor='k', label='Anomaly')
+ax2.hlines(y=0, xmin=date[0], xmax=date[-1], color='k', linewidth=0.8, zorder=-4)
 ax2.set_ylim(-0.6, 0.6)
 ax2.set_xticklabels([])
 ax2.set_ylabel('NDWI Anomaly')
 
+leg = ax2.legend(loc='upper right', ncol=1, handlelength=4.5, fontsize=tick_fontsize, columnspacing=2)
+#for legobj in leg.legendHandles:
+#    legobj.set_linewidth(4.0)
+
 #---formatting dates--------------------------------------
 ax2.xaxis.set_minor_locator(mdates.MonthLocator(interval=1))
 ax2.xaxis.set_minor_formatter(mdates.DateFormatter('%b'))
+ax2.get_xticklabels(which='minor')[22].set_color("red")
+
 ax2.xaxis.set_major_locator(mdates.MonthLocator(interval=1))
 ax2.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
 ax2.xaxis.set_major_locator(mdates.YearLocator())
@@ -84,7 +106,7 @@ ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y %b'))
 ax2.tick_params(axis='x', which='both', rotation=90)
 ax2.set_xlim(date[0], date[-1])
 
-ax2.vlines(x=pd.to_datetime('2019-05-01'), ymin=-1, ymax=1, colors='r', zorder=-4)
+ax2.vlines(x=pd.to_datetime('2019-05-01'), ymin=-1, ymax=1, colors='r', linewidth=11.0, zorder=-4, alpha=0.6)
 
 #----------saving figure---------------------
 figure_filename = r'code\ndvi_ndwi_figure.png'
